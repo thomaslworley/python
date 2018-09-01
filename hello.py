62dd3f60631cccb614e6e7c174056f385ec77a53
thomaslworley <42894068+thomaslworley@users.noreply.github.com>
2018-09-01 08:25:27 -0700

    changess

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ import sys
 
 def main():
 	name = input("Please type your name.")
-	if name == "Thomas":
-		print('Hello Thomas!')
+	if name is not None:
+		print('Hello %s!' % name)
 
 
 if __name__ == '__main__':
