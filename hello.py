bbe63d71800c522e644817365f9a0e4b8c9103f7
thomaslworley <42894068+thomaslworley@users.noreply.github.com>
2018-09-08 12:38:41 -0700

    commit

--- hello.py
+++ hello.py
@@ -6,10 +6,6 @@ def main():
 	if name is not None:
 		print('Hello %s!' % name)
 
-	name = input("What day is it?")
-	if name is not None:
-		print('Do you have anything to do this %s?' % name)
-
 
 	if __name__ == '__main__':
 		main()
