cade5e0a34bb778eb9e141ff81f0396b49cc101d
thomaslworley <42894068+thomaslworley@users.noreply.github.com>
2018-11-23 15:05:38 -0800

    Create Customer.py

---
+++ Customer.py
@@ -0,0 +1,18 @@
+import sys
+customer1 = 'this is the person or customer that wants lemons'
+customer2 = 'this is the person or customer that wants apples'
+customer3 = 'this is the person or customer that wants grapes'
+customer4 = 'this is the person or customer that wants limes'
+
+list1 = [customer1, customer2, customer3, customer4]
+
+raw_input('What do you want to find?')
+
+# if customer1.find(raw_input):
+#     print ('That is Customer 1')
+# if customer2.find(raw_input):
+#     print ('That is Customer 2')
+# if customer3.find(raw_input):
+#     print ('That is Customer 3')
+# if customer4.find(raw_input):
+#     print ('That is Customer 4')
